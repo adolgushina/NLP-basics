6a9240dcef95cff50080a2e26d41b3ab9c918453
adolgushina <50157615+adolgushina@users.noreply.github.com>
2019-05-03 13:26:49 -0700

    Create BagOfWords.py

---
+++ BagOfWords.py
@@ -0,0 +1,35 @@
+'''
+Language models are probabilistic computer models of language. We build and use these models to figure out the likelihood that a given sound, letter, word, or phrase will be used. Once a model has been trained, it can be tested out on new texts.
+
+One of the most common language models is the unigram model, a statistical language model commonly known as bag-of-words. As its name suggests, bag-of-words does not have much order to its chaos! What it does have is a tally count of each instance for each word.
+'''
+
+# importing regex and nltk
+import re, nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+# importing Counter to get word counts for bag of words
+from collections import Counter
+# importing a passage from Through the Looking Glass
+from looking_glass import looking_glass_text
+# importing part-of-speech function for lemmatization
+from part_of_speech import get_part_of_speech
+
+# Change text to another string:
+text = "hey hey hey Anna look out for the molester moon, the moon is scary!"
+
+cleaned = re.sub('\W+', ' ', text).lower()
+tokenized = word_tokenize(cleaned)
+
+stop_words = stopwords.words('english')
+filtered = [word for word in tokenized if word not in stop_words]
+
+normalizer = WordNetLemmatizer()
+normalized = [normalizer.lemmatize(token, get_part_of_speech(token)) for token in filtered]
+# Comment out the print statement below
+#print(normalized)
+
+# Define bag_of_looking_glass_words & print:
+bag_of_looking_glass_words = Counter(normalized)
+print(bag_of_looking_glass_words)
